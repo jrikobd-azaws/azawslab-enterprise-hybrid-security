42c56edfb60165d535e76f6cd96fd75a1a5ec66a
jrikobd-azaws <jrikobd@gmail.com>
2026-05-01 20:02:42 +0100

    P2a: add Key Vault secret flow to security module

--- terraform/modules/security/outputs.tf
+++ terraform/modules/security/outputs.tf
@@ -5,3 +5,12 @@
 output "key_vault_name" {
   value = azurerm_key_vault.security.name
 }
+
+output "local_admin_password_secret_name" {
+  value = azurerm_key_vault_secret.local_admin_password.name
+}
+
+output "generated_local_admin_password" {
+  value     = random_password.local_admin.result
+  sensitive = true
+}
