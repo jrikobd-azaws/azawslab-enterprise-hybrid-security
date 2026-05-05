35696d66bca4bafd1b7b8a5af8f3a4bd7d374583
jrikobd-azaws <jrikobd@gmail.com>
2026-05-05 14:13:11 +0100

    Add P7 Defender security contact

--- terraform/modules/security/variables.tf
+++ terraform/modules/security/variables.tf
@@ -12,3 +12,14 @@ variable "key_vault_secret_admin_object_ids" {
   description = "Object IDs that should have Key Vault secret administration permissions"
   type        = list(string)
 }
+
+variable "defender_security_contact_name" {
+  description = "Name of the Defender for Cloud security contact resource."
+  type        = string
+  default     = "default"
+}
+
+variable "defender_security_contact_email" {
+  description = "Email address used for Microsoft Defender for Cloud security notifications."
+  type        = string
+}
